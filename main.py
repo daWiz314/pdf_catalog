19db55888ea6e1a7a92b2f8b6a0c84c87b03b3ae
Harry <programmer6299@icloud.com>
2025-11-11 10:59:34 -0500

    Gave basic outline while I await on data

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
-from pypdf import PdfReader as pdfr
+
+
+userInput = ""
+partNumber = ""
+
+
+
+
+def clear_screen():
+    for i in range(100):
+        print()
+
+def get_part_number():
+    while True:
+        clear_screen()
+        print("Please enter the part number!")
+        userInput = input(">")
+        clear_screen()
+        print("You entered: " + userInput)
+        print("Is this correct? Y/N")
+        confirm = input(">")
+        if confirm.lower() == "y":
+            break
+        else:
+            continue
+    
+    return userInput.strip()
+
+def run_imports():
+    try:
+        from pypdf import PdfReader as pdfr
+    except ImportError:
+        print("This script needs pypdf!")
+        print("Please install through pip!")
+
+def main():
+    clear_screen()
+    run_imports()
+    print(get_part_number())
+
+main()
 
